c2325956594a71013d4892c06bb9e2a262306908
José Luis Ramírez Morales <sachielcode@gmail.com>
2021-04-18 15:59:19 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+namespace Example {
+    public class Echo : WebSocketBehavior {
+        protected override void OnMessage(MessageEventArgs e) {
+            var msg = System.Text.Encoding.UTF8.GetString(e.RawData);
+            Console.WriteLine("Got Message: " + msg);
+            Send(msg);
+        }
+    }
+
+    public class Program {
+        public static void Main(string[] args) {
+            var wssv = new WebSocketServer(8080);
+            wssv.AddWebSocketService<Echo>("/");
+            wssv.Start();
+            Console.Read();
+            wssv.Stop();
+        }
+    }
+}
